8980ed2178f9603d6ed2764b1178857b545f5760
dongupak <dongupak@bitbucket.org>
2014-05-17 16:24:32 +0900

    first commit

---
+++ AddSprite/GameLayer.h
@@ -0,0 +1,16 @@
+//
+//  GameLayer.h
+//  AddSprite
+//
+//  Created by ivis on 13. 6. 28..
+//  Copyright 2013년 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer {
+    
+}
+
+@end
---
+++ AddSprite/GameLayer.m
@@ -0,0 +1,14 @@
+//
+//  GameLayer.m
+//  AddSprite
+//
+//  Created by ivis on 13. 6. 28..
+//  Copyright 2013년 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameLayer.h"
+
+
+@implementation GameLayer
+
+@end
